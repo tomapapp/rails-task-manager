c01598c394df2198a2ca12bc896259ddc672e147
Tamas Papp <toma.papp92@gmail.com>
2020-03-10 15:10:54 +0100

    Rails day 2 - Ex. 2.: Rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
+
+# list tasks --> index GET /tasks
+# view task details  --> show GET /tasks/:id
+# add tasks --> new and create GET /tasks/new
+# edit tasks --> edit and update GET /tasks/:id/edit and PATCH /tasks/:id
+# remove tasks --> destroy DELETE /tasks/:id
